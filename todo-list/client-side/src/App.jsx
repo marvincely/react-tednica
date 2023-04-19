fc6aba9fcb3bab01f8e4036510fe14107014efcb
Marvin Cely <marvin.cely@mercadolibre.com.co>
2023-04-19 09:18:25 -0500

    Add scafolding, client & server

---
+++ todo-list/client-side/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.scss";
+import { Task } from "@components";
+
+const App = () => {
+  return (
+    <div className="App">
+      Hola Mundo <Task></Task>
+    </div>
+  );
+};
+
+export default App;
---
+++ todo-list/client-side/src/components/Task/Task.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import "./styles.scss";
+
+export const Task = () => {
+  return <div>Task</div>;
+};
